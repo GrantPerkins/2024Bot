1caac2c532bc6f75aec3bc4ffb2fb64f0efa3478
GrantPerkins <gcperk20@gmail.com>
2020-07-10 15:13:53 -0400

    New command to send messages as mod

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ TOKEN = ""
 with open("token.txt", 'r') as f:
     TOKEN = f.readline()
 
+
 def save_html(filename):
     response = urlopen(url)
     if response.getheader('Content-Type').split(";")[0] == 'text/html':
@@ -24,6 +25,18 @@ def save_html(filename):
 
 
 class Client(discord.Client):
+    def __init__(self):
+        super().__init__()
+        self.roles = {
+            "mod": 699644834629288007
+        }
+        self.user_ids = {
+            "elisabeth": 731214087740063886
+        }
+        self.channels = {
+            "faq-updates": 731214087740063886
+        }
+
     async def on_ready(self):
         print('Logged on as', self.user)
         # reset("faq.html")
@@ -33,20 +46,25 @@ class Client(discord.Client):
             return
 
         if message.content == '>ping':
-            print(message.author)
-            print(message.author.id)
             before = time()
             await message.channel.send('Pong!')
             ms = (time() - before) * 1000
             await message.channel.send('Ping took: {}ms'.format(int(ms)))
+        if ">send" in message.content and any([role.id == self.roles["mod"] for role in message.author.roles]):
+            cmd, channel, *text = message.content.split()
+            text = " ".join(text)
+            channel = self.get_channel(int(channel[2:-1]))
+            await channel.send(text)
 
-        if message.author.id == 696911603068829836 and any([i in message.content for i in ["tf", "walk", "mods"]]):
+        if message.author.id == self.user_ids["elisabeth"] and any(
+                [i in message.content for i in ["tf", "walk", "mods"]]):
             await message.channel.send("SHUT SHUT SHUT Elisabeth")
 
         save_html("current.html")
         if not filecmp.cmp("faq.html", "current.html"):
-            await self.get_channel(731214087740063886).send("WPI's FAQ changed. See:")
-            await self.get_channel(731214087740063886).send("https://www.wpi.edu/we-are-wpi/frequently-asked-questions")
+            await self.get_channel(self.channels["faq-updates"]).send("WPI's FAQ changed. See:")
+            await self.get_channel(self.channels["faq-updates"]).send(
+                "https://www.wpi.edu/we-are-wpi/frequently-asked-questions")
 
 
 client = Client()
